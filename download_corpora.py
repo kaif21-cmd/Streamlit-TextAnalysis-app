32e74ee841d38c7a97d2e1a069c705a86a5d7e04
ajay <ajaykrish.krishnanrb@gmail.com>
2023-05-30 20:00:06 +0530

    update nltk download punkt

--- download_corpora.py
+++ download_corpora.py
@@ -2,6 +2,7 @@ import subprocess
 
 subprocess.run(['python3', '-m', 'textblob.download_corpora'])
 subprocess.run(['python3', '-m', 'nltk.downloader', 'stopwords'])
+subprocess.run(['python3', '-m', 'nltk.downloader', 'punkt'])
 
 
 print("Working")
