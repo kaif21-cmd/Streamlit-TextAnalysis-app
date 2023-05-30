7de980eff2b0464f9992e70f16b6edc18d8698f7
ajay <ajaykrish.krishnanrb@gmail.com>
2023-05-30 19:13:05 +0530

    NLP app made with streamlit

---
+++ home.py
@@ -0,0 +1,132 @@
+import streamlit as st
+import altair as alt
+
+from utils import SentimentAnalysis, SpellingCorrection, PartsOfSpeechTagging, TextSummarize
+
+
+
+
+st.title('📝 Natural language processing')
+st.caption('''Natural language processing (NLP) refers to the branch of computer science and more specifically, the branch of artificial intelligence or AI concerned with giving computers the ability to understand text and spoken words in much the same way human beings can.''')
+
+tab1, tab2, tab3, tab4 = st.tabs(
+    tabs=[':blue[Sentiment Analysis]', ':green[Spelling Correction]', ':orange[Part-of-speech Tagging]', ':violet[Text Summarization]'])
+
+text_input = '''
+'The rain made things complicated' - Alonso
+Fernando Alonso who finished second at the Monaco Grand Prix: "It was difficult. We opted to start on hard tyres. We gave up the possibility in Turn One.
+
+"We tried to play a long game with the strategy but Max drove well on the mediums.
+
+"We didn’t have any chance but then in the end, the rain made things complicated and hard to drive around in.
+
+"I had no full power around the laps, the braking was fragile, it was not easy and I was surprised there was no safety car or incident. Everyone did a great job today."
+'''
+with tab1:
+    st.header('Sentiment Analysis')
+    text_to_analyze = st.text_area(label="Enter the text to analyse", value=text_input)
+    if text_to_analyze:
+        sentiment = SentimentAnalysis(text=text_to_analyze)
+        response = sentiment.get_mood(threshold=0.3)
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.metric(label="Sentiment Score", value=response.emoji, delta=round(response.sentiment,2))
+            st.divider()
+            c = alt.Chart(sentiment.get_word_count_df()).mark_circle().encode(x='words', y='count', size='count', color='count')
+            st.altair_chart(altair_chart=c, use_container_width=True)
+            
+        with col2:
+            analysis = sentiment.polarity_and_subjectivity()
+            st.caption(analysis)
+
+with tab2:
+    st.header('Spelling Correction')
+    text_to_spell_check = st.text_input(label="Enter the text to correct", value='I acknowlege that it is about 70% acurate')
+    if text_to_spell_check:
+        spellCheck = SpellingCorrection(text=text_to_spell_check)
+        response = spellCheck.spelling_correction()
+        st.write(response)
+        st.caption(spellCheck.redlines(), unsafe_allow_html=True)
+
+        st.divider()
+
+        explanation, keys = spellCheck.get_explanation()
+        
+        selected_keys = st.multiselect(
+            label=  "Spelling correction found" if keys else 'No correction required', 
+            options=keys, max_selections=5, 
+            default=keys[:4] if keys else None)
+        
+        for key in selected_keys:
+            response = explanation[key]
+
+            with st.expander(label=f':violet[Correction for {key}]', expanded=True):
+                for suggestion, confidence in response['correction']:
+                    st.caption(f'📢 :orange[Suggestion] : :green[{suggestion}] 🔢 :blue[confidence] : {round(confidence,2)}')
+
+                if response['definition']:
+                    st.subheader(f':violet[Definition] of {response["correction"][0][0]}')
+                    for defs in response['definition']:
+                        st.code(defs)
+
+
+
+
+
+with tab3:
+    st.header('Part-of-speech Tagging')
+    text_to_tag = st.text_input("Enter the text you want to tag", value='I work at Google and this is my sentence.')
+    if text_to_tag:
+        tagging = PartsOfSpeechTagging(text=text_to_tag)
+        df = tagging.parts_of_speech_tagging()
+        st.write(df)
+        
+        tag_map = tagging.parts_of_speech_map()
+        if tag_map:
+            keys = st.multiselect(label="Part-of-speech", options=list(tag_map.keys()), default=list(tag_map.keys())[:3], max_selections=5)
+                
+            for key in keys:
+                try:
+                    name = tagging.part_of_speech_help()[key]
+                except:
+                    name = ''
+                with st.expander(label=f":orange[{key}] | :green[{name}]", expanded=True):
+                    for word in tag_map[key]:
+                        st.code(word)
+
+
+what_is_nlp = '''
+What is natural language processing?
+Natural language processing (NLP) refers to the branch of computer science—and more specifically, the branch of artificial intelligence or AI—concerned with giving computers the ability to understand text and spoken words in much the same way human beings can.
+
+NLP combines computational linguistics—rule-based modeling of human language—with statistical, machine learning, and deep learning models. Together, these technologies enable computers to process human language in the form of text or voice data and to ‘understand’ its full meaning, complete with the speaker or writer’s intent and sentiment.
+
+NLP drives computer programs that translate text from one language to another, respond to spoken commands, and summarize large volumes of text rapidly—even in real time. There’s a good chance you’ve interacted with NLP in the form of voice-operated GPS systems, digital assistants, speech-to-text dictation software, customer service chatbots, and other consumer conveniences. But NLP also plays a growing role in enterprise solutions that help streamline business operations, increase employee productivity, and simplify mission-critical business processes.
+
+'''
+
+
+
+with tab4:
+    st.header("Text Summarization")
+    text_to_summarize = st.text_area(
+        label="Enter the text you want to summarize", value=what_is_nlp.strip())
+    if text_to_summarize:
+        summarizer = TextSummarize(text=text_to_summarize)
+        num = st.number_input(
+            label="How many sentences do you want in the summary?", min_value=1, step=1, value=3)
+        markup, summary = summarizer.get_summary(n=num)
+        st.subheader('Summary')
+
+        st.caption(summary)
+        
+        with st.expander(label=":orange[Highlights]", expanded=False):
+            st.caption(markup)
+
+        tokens = summarizer.get_token_frequency_df()
+        token_chart = alt.Chart(data=tokens).mark_circle().encode(x='Tokens', y='Weights', size='Weights', color='Weights')
+        st.altair_chart(altair_chart=token_chart, use_container_width=True)
+
+
+
---
+++ utils.py
@@ -0,0 +1,181 @@
+import re
+import string
+
+import pandas as pd
+import pathlib
+import matplotlib.pyplot as plt
+
+from nltk.corpus import stopwords
+from textblob import Word, WordList, Sentence
+from textblob import TextBlob
+from redlines import Redlines
+
+from heapq import nlargest
+from collections import defaultdict
+from itertools import compress
+from dataclasses import dataclass
+
+@dataclass
+class Mood:
+    emoji:str
+    sentiment:float
+
+class SentimentAnalysis:
+    def __init__(self, text:str):
+        self.text:str = text
+        self.blob = TextBlob(text)
+        
+
+    def get_mood(self, threshold:float=0.3)->Mood:
+        sentiment:float = self.blob.sentiment.polarity
+        friendly_threshold:float = threshold
+        hostile_threshold:float = -threshold
+
+        if sentiment >= friendly_threshold:
+            return Mood(emoji='😍😃😊🥰', sentiment=sentiment)
+        
+        elif sentiment <= hostile_threshold:
+            return Mood(emoji='😢😡😭😞', sentiment=sentiment)
+        else: 
+            return Mood(emoji='😐😑😒😕', sentiment=sentiment)
+
+
+    def polarity_and_subjectivity(self,)->str:
+        result:str = ''
+        for sentence in self.blob.sentences:
+            result += str(sentence)
+            sentiment = round(sentence.sentiment.polarity, 2)
+            subjectivity = round(sentence.sentiment.subjectivity, 2)
+            color = 'green' if sentiment>0 else 'red'
+            result += f'\n🔎 :blue[polarity]: :{color}[{sentiment}] 🚥 :orange[subjectivity]: :violet[{subjectivity}] \n\n'
+        return result
+
+    def get_word_count_df(self,)->pd.DataFrame:
+        word_count:defaultdict = self.blob.word_counts
+        return pd.DataFrame(data={'words':word_count.keys(), 'count':word_count.values()})
+
+
+
+class SpellingCorrection:
+    def __init__(self, text:str):
+        self.text:str = text.lower()
+        self.blob:TextBlob = TextBlob(text.lower())
+        self.correct:str =  str(TextBlob(text.lower()).correct())
+
+    def spelling_correction(self,)->str:
+        response = f'✅ {self.correct} 🖊'
+        return response
+    
+    def redlines(self)->str:
+        response = Redlines(source=self.text, test=self.correct).compare()
+        return '🔎'+ str(response)
+    
+    def get_explanation(self)->tuple[dict,list]:
+        input_text:list[str] = self.text.split()
+        correct_text:list[str] = self.correct.split()
+        mistake_mask:list[bool] = [True if a!=b else False for a,b in zip(input_text, correct_text)]
+        response:dict = {}
+        for i, mistake in enumerate(mistake_mask):
+            if mistake:
+                word_mistake = input_text[i]
+                word_correction = correct_text[i]
+
+                m = Word(word_mistake)
+                w = Word(word_correction)
+                
+                response[word_mistake] = {'correction':m.spellcheck(), 'definition': w.definitions} 
+        return response, list(compress(data=input_text, selectors=mistake_mask))
+        
+
+
+class PartsOfSpeechTagging:
+    def __init__(self, text:str):
+        self.text:str = text
+        self.blob:TextBlob = TextBlob(text)
+        self.pos:dict = dict(list(TextBlob(text=text).tags))
+        
+    def parts_of_speech_tagging(self,)->pd.DataFrame:
+        return pd.DataFrame(data=self.pos, index=['Part of Speech Tags'])
+    
+    def parts_of_speech_map(self,)->dict:
+        tags = defaultdict(lambda:[])
+        for word, tag in self.blob.tags:
+            tags[tag].append(word)
+        return tags
+    
+    def part_of_speech_help(self)->dict[str:str]:
+        pos_txt:list = pathlib.Path('./parts-of-speech-tags.txt').read_text().splitlines()
+        pos_tag_map:dict[str:str] = dict([ tuple(txt.split(sep='\t')) for txt in  pos_txt[2:] ])
+        return pos_tag_map
+
+
+class TextSummarize:
+    def __init__(self, text:str):
+        self.text:str = text
+        self.sentences:list[Sentence] = TextBlob(text).sentences
+        self.english_stopwords:list = stopwords.words('english')
+
+    def preprocessing(self,)->str:
+        res:str = re.sub(pattern='\s', repl=' ', string=self.text)
+        res = re.sub(pattern=f'[{string.punctuation}]', repl='', string=res)
+        res = res.lower()
+        return res
+    
+
+    def unique_token_frequency_scaled_map(self,)->dict[str:float]:
+        processed_text:str = self.preprocessing()
+        words:WordList = TextBlob(processed_text).words
+        word_count:defaultdict = TextBlob(processed_text).word_counts
+        tokens:set = set(words) - set(self.english_stopwords)
+        token_frequency:dict[str:int] = {k:word_count[k] for k in tokens}
+        W:int = max(token_frequency.values())
+        token_frequency_scaled:dict[str:float] = {k:v/W for k,v in token_frequency.items()}
+        return token_frequency_scaled
+    
+    def sentence_scores(self,)->defaultdict[str:float]:
+        weights:dict[str:float] = self.unique_token_frequency_scaled_map()
+        sentence_scores = defaultdict(lambda : 0)
+        for sentence in self.sentences:
+            for word in sentence.words:
+                try:
+                    score = weights[word.lower()]
+                except:
+                    score = 0
+                sentence_scores[str(sentence)] += score
+        return sentence_scores
+    
+    def get_summary(self, n=3)->tuple[str,str]:
+        score_dict:defaultdict[str:float] = self.sentence_scores()
+        summary:list = nlargest(n=n, iterable=score_dict, key=score_dict.get)
+        markup:str = ''
+        for sentence in self.sentences:
+            if str(sentence).strip() in summary:
+                markup += f':blue[{sentence}]'
+            else:
+                markup += str(sentence)
+            markup += '\n\n'
+
+        return markup, '\n\n'.join(summary)
+    
+    def get_token_frequency_df(self,)->pd.DataFrame:
+        weights:dict[str:float] = self.unique_token_frequency_scaled_map()
+        df = pd.DataFrame(data={'Tokens':weights.keys(),'Weights':weights.values()})
+        return df
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
+    
+    
+
+
